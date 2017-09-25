47abfe0c520d7a5570af4ea0049e4280db7be7fa
LTSZIN002 <LTSZIN002@COM004H>
2017-09-25 16:07:57 +0200

    first commit

---
+++ LTSZIN002_CloudWorshop/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace LTSZIN002_CloudWorshop
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ LTSZIN002_CloudWorshop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LTSZIN002_CloudWorshop.MvcApplication" Language="C#" %>
